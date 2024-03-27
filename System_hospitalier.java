9387bcc72a81620ac59cb18f65c2f2c729a8cfdf
dina4555 <ouazzanidina92@gmail.com>
2024-03-27 21:55:40 +0100

    first commit

---
+++ System_hospitalier.java
@@ -0,0 +1,46 @@
+package tp_git;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class System_hospitalier {
+
+    private List<Requête> listRequetes;
+
+    public System_hospitalier(List<Requête> listRequetes) {
+        super();
+        this.listRequetes = listRequetes;
+    }
+
+    public System_hospitalier() {
+        super();
+        listRequetes = new ArrayList<>();
+    }
+
+    public List<Requête> getListRequetes() {
+        return listRequetes;
+    }
+
+    public void setListRequetes(List<Requête> listRequetes) {
+        this.listRequetes = listRequetes;
+    }
+
+    public void recevoirRequete(Requête requete) {
+        listRequetes.add(requete);
+        String analyse =  analyserRequete(requete);
+        String comparaisonDonnees = comparerDonnes();
+        System.out.println(analyse);
+        System.out.println(comparaisonDonnees);
+    }
+
+    private String analyserRequete(Requête requête) {
+        //Analyse une requête et retourne une chaîne de caractères indiquant que la requête a été analysée.
+        return "requête analysée";
+    }
+
+    private String comparerDonnes() {
+        // Compare les données et retourne une chaîne de caractères indiquant qu'un cluster a été trouvé.
+        return "Cluster trouvé";
+    }
+
+}
